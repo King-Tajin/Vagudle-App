41cdf2cdcab661cdb3e75d3c24d1e5629bf78811
robotoloco3 <roland@shikoski.com>
2026-06-11 14:07:06 -0500

    Updated styling to reduce the web page feel.

--- android/app/src/main/java/dev/kingtajin/vagudle/MainActivity.java
+++ android/app/src/main/java/dev/kingtajin/vagudle/MainActivity.java
@@ -1,13 +1,30 @@
 package dev.kingtajin.vagudle;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getBridge().getWebView().setVerticalScrollBarEnabled(false);
-        getBridge().getWebView().setHorizontalScrollBarEnabled(false);
+
+        getWindow().setStatusBarColor(Color.BLACK);
+        getWindow().setNavigationBarColor(Color.BLACK);
+        WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView()).setAppearanceLightStatusBars(false);
+        WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView()).setAppearanceLightNavigationBars(false);
+
+        WebView webView = getBridge().getWebView();
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+
+        WebSettings settings = webView.getSettings();
+        settings.setSupportZoom(false);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
     }
 }
