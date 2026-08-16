5d4143f693eb23c54a626a83e53ff113871dbb01
robotoloco3 <roland@shikoski.com>
2026-08-16 09:23:53 -0500

    Disabled web-specific features.

---
+++ overrides/src/constants/settings.ts
@@ -0,0 +1,12 @@
+export const HARD_MODE_MAX_CHALLENGES = 9;
+export const NORMAL_MODE_MAX_CHALLENGES = 11;
+export const ALERT_TIME_MS = 2500;
+export const REVEAL_TIME_MS = 350;
+export const WELCOME_INFO_MODAL_MS = 350;
+export const MIN_LOADING_WORDS_TIME_MS = 250;
+export const DISCOURAGE_INAPP_BROWSERS = false;
+export const SHOW_KING_TAJIN_DISCLAIMER = false;
+export const NAVBAR_BRAND_URL = "https://King-Tajin.dev/yellow-skipper-games";
+export const NAVBAR_BRAND_URL_ACTIVITY =
+  "https://King-Tajin.dev/yellow-skipper-games";
+export const SOURCE_CODE_URL = "https://github.com/King-Tajin/Vagudle-App";
