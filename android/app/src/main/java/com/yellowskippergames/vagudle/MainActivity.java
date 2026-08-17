c6a17676ff16fadd43d48b11aecd0bde81de021e
robotoloco3 <roland@shikoski.com>
2026-08-17 01:44:53 -0500

    Fixed screen orientation.

--- android/app/src/main/java/com/yellowskippergames/vagudle/MainActivity.java
+++ android/app/src/main/java/com/yellowskippergames/vagudle/MainActivity.java
@@ -1,6 +1,8 @@
 package com.yellowskippergames.vagudle;
 
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -9,12 +11,21 @@ import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
 
+  private static final int LARGE_SCREEN_BREAKPOINT_DP = 600;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    boolean isLargeScreen =
+      getResources().getConfiguration().smallestScreenWidthDp >=
+      LARGE_SCREEN_BREAKPOINT_DP;
+    if (!isLargeScreen) {
+      setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
     super.onCreate(savedInstanceState);
 
-    getWindow().setStatusBarColor(Color.BLACK);
-    getWindow().setNavigationBarColor(Color.BLACK);
+    WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+    getWindow().setBackgroundDrawable(new ColorDrawable(Color.BLACK));
     WindowCompat.getInsetsController(
       getWindow(),
       getWindow().getDecorView()
